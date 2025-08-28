80aea9d6338f7241012c1e95bce5c7c7dc51f40a
Ihsan Fazil <ihsaninku@gmail.com>
2025-08-28 14:22:51 +0530

    Add support for serving files

--- server.c
+++ server.c
@@ -6,15 +6,35 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h> 
+#include <sys/stat.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define FILE_LOC "./files"
 
-char resp[] = "HTTP/1.0 200 OK\r\n"
-"Server: webserver-c\r\n"
-"Content-type: text/html\r\n\r\n"
-"<html>Hello World</html>\r\n";
+const char* get_mime_type(const char* path) {
 
+    char* ext = strrchr(path, '.');
+    if (!ext) return "text/plain";
+    if (strcasecmp(ext, ".html") == 0) {
+        return "text/html";
+    }
+    else if (strcasecmp(ext, ".css") == 0) {
+        return "text/css";
+    }
+    else if (strcasecmp(ext, ".jpg") == 0) {
+        return "image/jpeg";
+    }
+    else if (strcasecmp(ext, ".png") == 0) {
+        return "image/png";
+    }
+    else if (strcasecmp(ext, ".js") == 0) {
+        return "application/javascript";
+    }
+    else {
+        return "application/octet-stream";
+    }
+}
 
 void* handle_client(void* arg) {
     int newsockfd = *(int*)arg;
@@ -28,14 +48,51 @@ void* handle_client(void* arg) {
 
     int valread = read(newsockfd, buffer, BUFFER_SIZE);
 
+    buffer[valread] = "\0";
     char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
     sscanf(buffer, "%s %s %s", method, uri, version);
     printf("[%s:%u] %s %s %s\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), method, version, uri);
 
-    int valwrite = write(newsockfd, resp, strlen(resp));
+    char path[BUFFER_SIZE];
+    if (strcmp(uri, "/") == 0) {
+        snprintf(path, sizeof(path), "%s/index.html", FILE_LOC);
+    }
+    else {
+        snprintf(path, sizeof(path), "%s%s", FILE_LOC, uri);
+    }
 
+    FILE* file = fopen(path, "rb");
+    if (file) {
+        struct stat st;
+        stat(path, &st);
+        size_t filesize = st.st_size;
+
+        char header[BUFFER_SIZE];
+        snprintf(header, sizeof(header),
+            "HTTP/1.0 200 OK\r\n"
+            "Server: webserver-c\r\n"
+            "Content-Length: %zu\r\n"
+            "Content-Type: %s\r\n\r\n",
+            filesize, get_mime_type(path));
+        write(newsockfd, header, strlen(header));
+
+        char filebuf[BUFFER_SIZE];
+        size_t n;
+        while ((n = fread(filebuf, 1, sizeof(filebuf), file)) > 0) {
+            write(newsockfd, filebuf, n);
+        }
+        fclose(file);
+    } else {
+        char* not_found = "HTTP/1.0 404 Not Found\r\n"
+            "Server: webserver-c\r\n"
+            "Content-type: text/html\r\n\r\n"
+            "<html>File not found</html>\r\n";
+        write(newsockfd, not_found, strlen(not_found));
+    }
     close(newsockfd);
+    return NULL;
 }
+    
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
